93d024664a8ae8934017ed1b6b269f56c8560a54
Etrusco <ademarchi74@hotmail.com>
2014-03-25 10:55:39 -0400

    heroku

--- public/js/n.js
+++ public/js/n.js
@@ -27,8 +27,7 @@ function __nodelog__() {
     args += '&w='+e(w); args += '&h='+e(h); args += '&a='+e(a);
     args += '&hl='+e(hl); 
     //img.src='http://nodelog-c9-etrusco.c9.io/nodelog'+args;
-    //img.src='http://nodelogapp.herokuapp.com/nodelog'+args;
-    img.src=window.location.hostname+'/nodelog'+args;
+    img.src='http://nodelogapp.herokuapp.com/nodelog'+args;
     img.onload = function() { return; };
     
 }
